49d790bb84abd36effb73768135282fd9a93df20
Margaret O'Neill <margaret@oneillfish.com>
2019-03-07 21:01:48 -0500

    added buttons and functions for moving through fortunes

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,47 @@
 //array of fortunes.  edited from http://yerkee.com/api/fortune
 //i inserted <br> so it would fit on my fortune display
-const fortunes = [{"fortune":"Play Rogue, visit exotic locations, \r\nmeet strange creatures and kill them."},
-{"fortune":"While having never invented a sin, \r\nI'm trying to perfect several."},
-{"fortune":"Do not believe in miracles -- rely on them."},
-{"fortune":"giraffiti:Vandalism spray-painted very, very high."},
-{"fortune":"Algebraic symbols are used when you do not know \r\nwhat you are talking about.- Philippe Schnoebelen"},
-{"fortune":"No one gets too old to learn \r\na new way of being stupid.\""},
-{"fortune":"Yes, but every time I try to see things your way,\r\n I get a headache."},
-{"fortune":"serendipity, n.:he process by which human knowledge is advanced."},
-{"fortune":"Life is not for everyone."},
-{"fortune":"You can make it illegal, \r\nbut you can't make it unpopular."}]
+const fortunes = [{"fortune":"1 Play Rogue, visit exotic locations, \r\nmeet strange creatures and kill them."},
+{"fortune":"2 While having never invented a sin, \r\nI'm trying to perfect several."},
+{"fortune":"3 Do not believe in miracles -- rely on them."},
+{"fortune":"4 giraffiti:Vandalism spray-painted very, very high."},
+{"fortune":"5 Algebraic symbols are used when you do not know \r\nwhat you are talking about.- Philippe Schnoebelen"},
+{"fortune":"6 No one gets too old to learn \r\na new way of being stupid.\""},
+{"fortune":"7 Yes, but every time I try to see things your way,\r\n I get a headache."},
+{"fortune":"8 serendipity, n.:he process by which human knowledge is advanced."},
+{"fortune":"9 Life is not for everyone."},
+{"fortune":"10 You can make it illegal, \r\nbut you can't make it unpopular."}]
 
+//need variable to hold current fortune
+let currentFortune = -1;
 //get pointers to the button, and cookie text element
 
 const cookieText = document.querySelector('[data-fortune]');
-const buttonFortune = document.querySelector('[data-button]');
+const buttonFortuneRandom = document.querySelector('[data-button-random]');
+const buttonFortuneNext = document.querySelector('[data-button-forward]');
+const buttonFortunePrev = document.querySelector('[data-button-reverse]');
 
-randomNumber = parseInt(Math.random() * fortunes.length)
-console.log(randomNumber);
+//assign event listeners to the buttons
+buttonFortuneRandom.addEventListener('click',updateRandom);
+buttonFortuneNext.addEventListener('click',updateNext);
+buttonFortunePrev.addEventListener('click',updatePrev);
 
-cookieText.textContent = fortunes[randomNumber].fortune;
+
+function updateText (fortuneNumber) {
+    cookieText.textContent = fortunes[fortuneNumber].fortune;
+};
+function updateNext() {
+    currentFortune = (currentFortune + 1) % fortunes.length;
+    console.log(currentFortune);
+    updateText(currentFortune);
+}
+function updatePrev() {
+    currentFortune = (currentFortune - 1 + fortunes.length) % fortunes.length;
+    console.log(currentFortune);
+    updateText(currentFortune);
+}
+function updateRandom() {
+    randomNumber = parseInt(Math.random() * fortunes.length)
+    // console.log(randomNumber);
+    updateText(randomNumber);
+    currentFortune = randomNumber;
+}
